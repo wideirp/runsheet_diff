ea972806352eb814fe19a452db45c1ae7ea0b59c
wideirp <wideirp@gmail.com>
2024-02-06 14:19:40 -0600

    change row param to row_start

--- runsheet_diff.py
+++ runsheet_diff.py
@@ -6,22 +6,20 @@ import openpyxl as xl
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--runsheet")
-parser.add_argument("--sheet")
-parser.add_argument("--col")
-parser.add_argument("--rows")
-parser.add_argument("--instruments")
+parser.add_argument("--runsheet", help="path to runsheet")
+parser.add_argument("--sheet", help="name of sheet; usually Runsheet")
+parser.add_argument(
+    "--col", help="column to get Volume & Page info; usually C")
+parser.add_argument("--row_start", help="first row of instruments; usually 34")
+parser.add_argument("--instruments", help="path to instruments folder")
 
 args = parser.parse_args()
-
-row_start, row_end = args.rows.split(":")
-row_start, row_end = [int(row_start), int(row_end)]
 
 rs = xl.load_workbook(args.runsheet)
 wb = rs[args.sheet]
 
 rs_fns = [cell.value.strip()
-          for cell in wb[args.col] if (cell.value and cell.row >= row_start and cell.row <= row_end)]
+          for cell in wb[args.col] if (cell.value and cell.row >= int(args.row_start) and cell.row <= 2000)]
 
 inst_fns = [Path(fn).stem.strip()
             for fn in os.listdir(args.instruments) if fn[0] != '.']
@@ -34,7 +32,6 @@ rs_missing = [fn for fn in inst_fns if fn not in rs_fns]
 # OUTPUT
 # Missing from Runsheet    Missing from Instruments Folder
 temp = '{0:25}  {1}'
-
 
 print(temp.format(f"Runsheet Total: {
       len(rs_fns)}", f"Instrument Total: {len(inst_fns)}"))
